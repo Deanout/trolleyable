8a822631e2a3cfd16861c5b899dd16cef54e18c4
Dean DeHart <DeHartDean@gmail.com>
2022-02-14 14:08:00 -0500

    Add Material UI

---
+++ src/components/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import MenuAppBar from './NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {MenuAppBar()}
+      </header>
+      <main className="App-main">
+    
+      </main>
+      
+    </div>
+  );
+}
+
+export default App;
